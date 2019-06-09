c234c4c5f282ac730f40f9c0be4b926d43dac546
eTimokhov <e.timokhov@gmail.com>
2019-06-09 20:46:59 +0300

    Cart, CartItem, HttpSessionCartService

--- core/src/main/java/com/es/core/cart/Cart.java
+++ core/src/main/java/com/es/core/cart/Cart.java
@@ -1,4 +1,21 @@
 package com.es.core.cart;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Scope(value="session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Cart {
+    private List<CartItem> cartItems;
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
 }
--- core/src/main/java/com/es/core/cart/HttpSessionCartService.java
+++ core/src/main/java/com/es/core/cart/HttpSessionCartService.java
@@ -1,28 +1,58 @@
 package com.es.core.cart;
 
+import com.es.core.model.phone.Phone;
+import com.es.core.model.phone.PhoneDao;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class HttpSessionCartService implements CartService {
+    @Resource
+    private Cart cart;
+
+    @Resource
+    private PhoneDao phoneDao;
+
     @Override
     public Cart getCart() {
-        throw new UnsupportedOperationException("TODO");
+        return cart;
     }
 
     @Override
     public void addPhone(Long phoneId, Long quantity) {
-        throw new UnsupportedOperationException("TODO");
+        Phone phone = phoneDao.get(phoneId).orElseThrow(() -> new IllegalArgumentException("Phone with id " + phoneId + " not found."));
+        Optional<CartItem> optionalCartItem = cart.getCartItems().stream()
+                .filter(ci -> ci.getPhone().getId().equals(phoneId))
+                .findAny();
+        if (optionalCartItem.isPresent()) {
+            CartItem cartItem = optionalCartItem.get();
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+        } else {
+            cart.getCartItems().add(new CartItem(phone, quantity));
+        }
     }
 
     @Override
     public void update(Map<Long, Long> items) {
-        throw new UnsupportedOperationException("TODO");
+        for (Map.Entry<Long, Long> entry : items.entrySet()) {
+            Optional<CartItem> optionalCartItem = cart.getCartItems().stream()
+                    .filter(ci -> ci.getPhone().getId().equals(entry.getKey()))
+                    .findAny();
+            if (optionalCartItem.isPresent()) {
+                optionalCartItem.get().setQuantity(entry.getValue());
+            } else {
+                addPhone(entry.getKey(), entry.getValue());
+            }
+        }
     }
 
     @Override
     public void remove(Long phoneId) {
-        throw new UnsupportedOperationException("TODO");
+        cart.getCartItems().removeIf(ci -> ci.getPhone().getId().equals(phoneId));
     }
+
 }
